198a4449790803548972d84e14583ba3dd4f6175
Gazdag András <gazdag.andras@gmail.com>
2016-02-19 17:35:04 +0100

    index.php hozzáadása, utf-8 karakterkódolással

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Alkalmazottak nyilvántartása</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+<h1>Alkalmazottak nyilvántartása</h1>
+
+
+</body>
+</html> 
