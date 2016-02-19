d6aaf38c75f7b8e9bd85aa2bf7ad5f8017415917
Gazdag András <gazdag.andras@gmail.com>
2016-02-19 18:09:42 +0100

    HTML táblázat hozzáadása

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Alkalmazottak nyilvántartása</title>
-  <meta charset="utf-8">
-</head>
-<body>
+    <head>
+        <title>Alkalmazottak nyilvántartása</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
 
-<h1>Alkalmazottak nyilvántartása</h1>
+        <h1>Alkalmazottak nyilvántartása</h1>
 
+        <h2>Alkalmazottak adatai</h2>
 
-</body>
+        <table border="1">
+            <tr>
+                <th>Név</th>
+                <th>Cím</th>
+                <th>Telefon</th>
+                <th>e-mail</th>
+            </tr>
+            <tr>
+                <td>Kiss Jenő</td>
+                <td>7400 Kaposvár, Fő utca ...</td>
+                <td>70/6767</td>
+                <td>kiss@jeno.hu</td>
+            </tr>
+        </table>
+
+    </body>
 </html> 
